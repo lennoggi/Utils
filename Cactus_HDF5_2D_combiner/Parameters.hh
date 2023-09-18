a6c91752145149f92741cbc0dcb2a16e17b57ebc
Lorenzo Ennoggi <lorenzo.ennoggi@gmail.com>
2023-09-18 12:05:13 -0500

    Add clean.sh

--- Cactus_HDF5_2D_combiner/Parameters.hh
+++ Cactus_HDF5_2D_combiner/Parameters.hh
@@ -2,13 +2,16 @@
 #define PARAMETERS_HH
 
 
-#define INPUT_FILE_1 "/scratch3/07825/lennoggi/BBH_handoff_McLachlan_pp08/output-0005/HDF5_2D/smallb2.xz.h5"
-#define INPUT_FILE_2 "/scratch3/07825/lennoggi/BBH_handoff_McLachlan_pp08/output-0005/HDF5_2D/rho_b.xz.h5"
+#define INPUT_FILE_1 "/scratch3/07825/lennoggi/BBH_handoff_McLachlan_pp08/output-0007/HDF5_2D/smallb2.xz.h5"
+#define INPUT_FILE_2 "/scratch3/07825/lennoggi/BBH_handoff_McLachlan_pp08/output-0007/HDF5_2D/rho_b.xz.h5"
+//#define INPUT_FILE_2 "/scratch3/07825/lennoggi/BBH_handoff_McLachlan_pp08/output-0007/HDF5_2D/P.xz.h5"
 
-#define OUTPUT_FILE "/scratch3/07825/lennoggi/magnetization.xz.h5"
+#define OUTPUT_FILE "/scratch3/07825/lennoggi/BBH_pp08_magnetization_output-0007.xz.h5"
+//#define OUTPUT_FILE "/scratch3/07825/lennoggi/BBH_pp08_b2_over_2P_output-0007.xz.h5"
 
 #define DATASET_BASENAME_1 "SMALLBPOYNET::smallb2"
 #define DATASET_BASENAME_2 "ILLINOISGRMHD::rho_b"
+//#define DATASET_BASENAME_2 "ILLINOISGRMHD::P"
 
 // Choose among "linear combination", "product", or "ratio"
 #define OPERATION "ratio"
@@ -18,7 +21,8 @@
 #define LINCOMB_A2 1.
 
 // Overall multiplicative factor when computing a product or ratio
-#define PROD_RATIO_FAC 0.5
+//#define PROD_RATIO_FAC 1.
+#define PROD_RATIO_FAC 1.
 
 /* Tip: just make the code verbose during debug, otherwise the execution is
  * slowed down                                                                  */
---
+++ Cactus_HDF5_2D_combiner/clean.sh
@@ -0,0 +1,1 @@
+rm -f Combine_Cactus_HDF5.o Main.o Cactus_HDF5_2D_combiner_exe
